acc1b938f1acf8e89207fc474600e3bc2620a6ab
Emerson <eemr3@yahoo.com.br>
2022-02-26 16:15:03 -0300

    feat(loginStorage): cria funçoes para gardar dados do login no local storage

---
+++ src/functions/loginStorage.js
@@ -0,0 +1,5 @@
+export const setMealsTokenInStorage = (token) => localStorage.setItem('token', token);
+
+export const setCocktailsTokenInStorage = (token) => localStorage.setItem('token', token);
+
+export const setEmailInStorage = (email) => localStorage.setItem('email', email);
