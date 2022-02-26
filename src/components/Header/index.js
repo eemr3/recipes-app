4c12651931c6152e1a8738396513f16e14e60de4
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-02-26 18:07:21 -0300

    feat(Header): implementado o css do header no index.css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './styles.css';
 
 function Header({ title, isSearch }) {
   return (
--- src/pages/Drinks/index.js
+++ src/pages/Drinks/index.js
@@ -4,7 +4,7 @@ import Header from '../../components/Header';
 function Drinks() {
   return (
     <div>
-      <Header title="Bebidas" />
+      <Header title="Bebidas" isSearch />
     </div>
   );
 }
