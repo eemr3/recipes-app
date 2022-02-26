2b47d3c55589d26a796b00b0dee37219b608f790
Rafael Santos <68519691+rafaelsantosmg@users.noreply.github.com>
2022-02-26 15:33:18 -0300

    Revert "iniando projeto #VQV :rocket"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
-import Routes from './routes/Routes';
+import rockGlass from './images/rockGlass.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Routes />
+    <div className="meals">
+      <span className="logo">TRYBE</span>
+      <object
+        className="rocksGlass"
+        type="image/svg+xml"
+        data={ rockGlass }
+      >
+        Glass
+      </object>
+    </div>
   );
 }
 
