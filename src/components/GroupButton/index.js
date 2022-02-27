7b9f429a544aa8bc0358475928a5261b8cef82c5
Emerson <eemr3@yahoo.com.br>
2022-02-26 23:29:10 -0300

    fix(Style): corrige bug de render do botão All

--- src/components/GroupButton/index.js
+++ src/components/GroupButton/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { requestCategoryFoods } from '../../services/api';
+import Loading from '../Loading';
 
 const FIVE = 5;
 
@@ -29,30 +30,32 @@ function GroupButton({ handleClick, route }) {
 
   return (
 
-    <div className="flex justify-center flex-wrap">
-      <button
-        type="button"
-        className="px-6 py-2 text-sm transition-colors duration-300
-          rounded-full shadow-md text-violet-100 bg-orange-500
-          hover:bg-orange-600 shadow-orange-400 w-28 m-1"
-        onClick={ handleClick }
-      >
-        All
-      </button>
-      {categoryBtn.map(({ strCategory }) => (
+    categoryBtn.length === 0 ? (<Loading />) : (
+      <div className="flex justify-center flex-wrap">
         <button
-          key={ strCategory }
           type="button"
           className="px-6 py-2 text-sm transition-colors duration-300
-          rounded-full shadow-md text-violet-100 bg-orange-500
-          hover:bg-orange-600 shadow-orange-400 w-28 m-1"
+            rounded-full shadow-md text-violet-100 bg-orange-500
+            hover:bg-orange-600 shadow-orange-400 w-28 m-1"
           onClick={ handleClick }
         >
-          {strCategory.includes('Unknown') ? strCategory.split('/')[0]
-            : strCategory.split(' ')[0]}
+          All
         </button>
-      ))}
-    </div>
+        {categoryBtn.map(({ strCategory }) => (
+          <button
+            key={ strCategory }
+            type="button"
+            className="px-6 py-2 text-sm transition-colors duration-300
+            rounded-full shadow-md text-violet-100 bg-orange-500
+            hover:bg-orange-600 shadow-orange-400 w-28 m-1"
+            onClick={ handleClick }
+          >
+            {strCategory.includes('Unknown') ? strCategory.split('/')[0]
+              : strCategory.split(' ')[0]}
+          </button>
+        ))}
+      </div>)
+
   );
 }
 
