0b1879d8308f6eb5a817bba55653798bb6e83bed
Emerson <eemr3@yahoo.com.br>
2022-02-28 18:44:46 -0300

    feat(DrinkDetails): cria página de DrinkDetails

--- src/pages/DrinkDetails/index.js
+++ src/pages/DrinkDetails/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { requestDetailDrink } from '../../services/api';
+import CardDatailsAndInProgressDrinks
+from '../../components/CardDatailsAndInProgressDrinks';
 
 export default function DrinkDetails() {
+  const { id } = useParams();
+  const [drink, setDrink] = useState([]);
+
+  useEffect(() => {
+    const getDrinkDetail = async () => {
+      const data = await requestDetailDrink(id);
+      setDrink(data);
+    };
+    getDrinkDetail();
+  }, [id]);
+
   return (
-    <div>DrinksDetails</div>
+    drink.length > 0 && <CardDatailsAndInProgressDrinks recipe={ drink[0] } inDetail />
   );
 }
