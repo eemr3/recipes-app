9d9826fb4724ac39265a0234c947704627e5a53e
Emerson <eemr3@yahoo.com.br>
2022-02-28 18:44:21 -0300

    feat(DrinkInProgress): cria página de DrinkInProgress

--- src/pages/DrinksInProgress/index.js
+++ src/pages/DrinksInProgress/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { requestDetailDrink } from '../../services/api';
+import CardDatailsAndInProgressDrinks
+from '../../components/CardDatailsAndInProgressDrinks';
 
 function DrinksInProgress() {
+  const { id } = useParams();
+  const [inProgressData, setInProgressData] = useState([]);
+
+  useEffect(() => {
+    const getDetailDrink = async () => {
+      const response = await requestDetailDrink(id);
+      setInProgressData(response);
+    };
+    getDetailDrink();
+  }, [id]);
+
   return (
-    <div> </div>
+    inProgressData.length > 0 && (
+      <CardDatailsAndInProgressDrinks
+        recipe={ inProgressData[0] }
+        inProgress
+      />)
   );
 }
 
