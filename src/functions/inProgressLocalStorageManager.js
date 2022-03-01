b1cebc154f7a97156864230526ce9c835e55064d
Emerson <eemr3@yahoo.com.br>
2022-03-01 20:41:15 -0300

    refactor(inProgressLocalStorageManager): refatora lógica para salva receitas no localstorage

--- src/functions/inProgressLocalStorageManager.js
+++ src/functions/inProgressLocalStorageManager.js
@@ -1,12 +1,24 @@
-const localStorageManagerIngredients = (id, name) => {
-  localStorage.setItem('inProgressRecipes', JSON.stringify({
-    cocktails: {
-      [id]: [name],
-    },
-    meals: {
-      [id]: [name],
-    },
-  }));
+const localStorageManagerIngredients = (id, name, type, prevState) => {
+  const key = JSON.parse(localStorage.getItem('inProgressRecipes')) || [];
+
+  if (prevState) {
+    localStorage.setItem('inProgressRecipes', JSON.stringify({
+      ...key,
+      [type]: {
+        ...key[type],
+        [id]: key[type]
+        && key[type][id] ? [...key[type][id], name] : [name],
+      },
+    }));
+  } else {
+    localStorage.setItem('inProgressRecipes', JSON.stringify({
+      ...key,
+      [type]: {
+        ...key[type],
+        [id]: key[type][id].filter((item) => item !== name),
+      },
+    }));
+  }
 };
 
 export default localStorageManagerIngredients;
