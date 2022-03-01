4d404479feb7b86c3fc4c734f5ff6fe275f47c60
Emerson <eemr3@yahoo.com.br>
2022-03-01 18:33:56 -0300

    feat(sliderSettings):  arquivo de configurações do carrousel

---
+++ src/components/Carousel/sliderSettings.js
@@ -0,0 +1,31 @@
+const sliderSettings = {
+  arrows: false,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+  ],
+};
+
+export default sliderSettings;
