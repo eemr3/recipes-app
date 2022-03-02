a2cb3cd6d2a10eb6892672c66d0c3ea0d2e3e2be
Emerson <eemr3@yahoo.com.br>
2022-03-02 19:59:41 -0300

    feat(Explore): cria tela de explorar

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function Explore() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar" />
+      <div className="flex items-center justify-center mt-5 container max-w-5xl mx-auto">
+        <button
+          type="button"
+          className="px-6 py-2 text-sm transition-colors duration-300
+            rounded-full shadow-md text-violet-100 bg-orange-500
+            hover:bg-orange-600 shadow-orange-400 w-full m-1"
+          onClick={ () => history.push('/explore-foods') }
+        >
+          Explorar Comidas
+
+        </button>
+        <button
+          type="button"
+          className="px-6 py-2 text-sm transition-colors duration-300
+            rounded-full shadow-md text-violet-100 bg-orange-500
+            hover:bg-orange-600 shadow-orange-400 w-full m-1"
+          onClick={ () => history.push('/explore-drinks') }
+        >
+          Explorar Bebidas
+
+        </button>
+      </div>
       <Footer />
     </div>
   );
