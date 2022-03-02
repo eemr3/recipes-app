45bb0d9f94bf0c6ad27e8d0953c22ecc1ac8f4be
Emerson <eemr3@yahoo.com.br>
2022-03-02 20:16:27 -0300

    feat(ExploreFoods/ExploreDrinks): cria telas de explorar comida e explorar bebida

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -8,26 +8,27 @@ function Explore() {
   return (
     <div>
       <Header title="Explorar" />
-      <div className="flex items-center justify-center mt-5 container max-w-5xl mx-auto">
+      <div
+        className="flex flex-col
+        items-center justify-center mt-5 container max-w-4xl mx-auto"
+      >
         <button
           type="button"
           className="px-6 py-2 text-sm transition-colors duration-300
             rounded-full shadow-md text-violet-100 bg-orange-500
-            hover:bg-orange-600 shadow-orange-400 w-full m-1"
+            hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
           onClick={ () => history.push('/explore-foods') }
         >
           Explorar Comidas
-
         </button>
         <button
           type="button"
           className="px-6 py-2 text-sm transition-colors duration-300
             rounded-full shadow-md text-violet-100 bg-orange-500
-            hover:bg-orange-600 shadow-orange-400 w-full m-1"
+            hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
           onClick={ () => history.push('/explore-drinks') }
         >
           Explorar Bebidas
-
         </button>
       </div>
       <Footer />
--- src/pages/ExploreDrinks/index.js
+++ src/pages/ExploreDrinks/index.js
@@ -6,6 +6,30 @@ function ExploreDrinks() {
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <div
+        className="flex flex-col
+        items-center justify-center mt-5 container max-w-4xl mx-auto"
+      >
+        <button
+          type="button"
+          className="px-6 py-2 text-sm transition-colors duration-300
+            rounded-full shadow-md text-violet-100 bg-orange-500
+            hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
+          // onClick={ () => history.push('/explore-foods') }
+        >
+          Por Ingredientes
+
+        </button>
+        <button
+          type="button"
+          className="px-6 py-2 text-sm transition-colors duration-300
+            rounded-full shadow-md text-violet-100 bg-orange-500
+            hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
+          // onClick={ () => history.push('/explore-drinks') }
+        >
+          Surprienda-me
+        </button>
+      </div>
       <Footer />
     </div>
   );
