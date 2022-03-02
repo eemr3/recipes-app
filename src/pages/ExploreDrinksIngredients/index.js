e638a0ddd3c66d0a1e4159e2746d1d239b43d3ed
Emerson <eemr3@yahoo.com.br>
2022-03-02 20:27:39 -0300

    feat(ExploreFoods/ExploreDrinks): adicina função para mudar de rota

---
+++ src/pages/ExploreDrinksIngredients/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ExploreDrinksIngredients() {
+  return (
+    <div>
+      <h1>Explorer Drinks Ingredients</h1>
+    </div>
+  );
+}
+
+export default ExploreDrinksIngredients;
--- src/pages/ExploreFoods/index.js
+++ src/pages/ExploreFoods/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function ExploreFoods() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar Comidas" />
@@ -15,7 +17,7 @@ function ExploreFoods() {
           className="px-6 py-2 text-sm transition-colors duration-300
             rounded-full shadow-md text-violet-100 bg-orange-500
             hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
-          // onClick={ () => history.push('/explore-foods') }
+          onClick={ () => history.push('/explore-foods-ingredients') }
         >
           Por Ingredientes
 
@@ -25,7 +27,7 @@ function ExploreFoods() {
           className="px-6 py-2 text-sm transition-colors duration-300
             rounded-full shadow-md text-violet-100 bg-orange-500
             hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
-          // onClick={ () => history.push('/explore-drinks') }
+          onClick={ () => history.push('/explore-foods-nationality') }
         >
           Por Nacionalidade
         </button>
