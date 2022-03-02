fa833cccddba4a01525e9e5cd3d6150dccf180c2
Emerson <eemr3@yahoo.com.br>
2022-03-02 20:57:09 -0300

    feat(ExploreDrinks) adiciona funcionalidade de gerar receita de bebida randômico

--- src/pages/ExploreDrinks/index.js
+++ src/pages/ExploreDrinks/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { requestRandomRecipesDrinks } from '../../services/api';
 
 function ExploreDrinks() {
   const history = useHistory();
+  const [randomDrinks, setRandomDrinks] = useState([]);
+
+  useEffect(() => {
+    const getRandomRecipeCocktail = async () => {
+      const response = await requestRandomRecipesDrinks();
+      setRandomDrinks(response);
+    };
+    getRandomRecipeCocktail();
+  }, []);
+
+  const handleClickRandomDrinks = () => {
+    history.push(`/drinks/${randomDrinks[0].idDrink}`);
+  };
+
   return (
     <div>
       <Header title="Explorar Bebidas" />
@@ -27,7 +42,7 @@ function ExploreDrinks() {
           className="px-6 py-2 text-sm transition-colors duration-300
             rounded-full shadow-md text-violet-100 bg-orange-500
             hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
-          // onClick={ () => history.push('/explore-drinks') }
+          onClick={ handleClickRandomDrinks }
         >
           Surprienda-me
         </button>
