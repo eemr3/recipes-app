3fb24b66392226729dfee383257e79033b9bb17b
Emerson <eemr3@yahoo.com.br>
2022-03-02 20:52:42 -0300

    feat(ExploreFoods): aciona funcionalidade de gerar uma recita de comida randômico

--- src/pages/ExploreFoods/index.js
+++ src/pages/ExploreFoods/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { requestRandomRecipesFoods } from '../../services/api';
 
 function ExploreFoods() {
   const history = useHistory();
+  const [randomMeals, setRandomMeals] = useState([]);
+
+  useEffect(() => {
+    const getRandomRecipeMeal = async () => {
+      const response = await requestRandomRecipesFoods();
+      setRandomMeals(response);
+    };
+    getRandomRecipeMeal();
+  }, []);
+  const handleClickRandomRecipe = () => {
+    history.push(`/foods/${randomMeals[0].idMeal}`);
+  };
+
   return (
     <div>
       <Header title="Explorar Comidas" />
@@ -36,7 +50,7 @@ function ExploreFoods() {
           className="px-6 py-2 text-sm transition-colors duration-300
             rounded-full shadow-md text-violet-100 bg-orange-500
             hover:bg-orange-600 shadow-orange-400 w-3/4 m-1"
-          // onClick={ () => history.push('/explore-drinks') }
+          onClick={ handleClickRandomRecipe }
         >
           Surprienda-me
         </button>
